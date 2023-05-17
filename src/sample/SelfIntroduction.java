65c0a9011f053270c127c205bc57320e52efc850
kamino <taishi8976@gmail.com>
2023-05-17 10:51:22 +0900

    1.3 Java導入

---
+++ src/sample/SelfIntroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//氏名
+		String name = "神野　太志";
+		//年齢
+		int age = 28;
+		//趣味
+		String hobby = "料理研究";
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		
+		System.out.print("私の名前は" + name + "です。\n");
+		System.out.print("年齢は" + age + "です。\n");
+		System.out.print("趣味は" + hobby + "です。\n");
+
+	}
+
+}
